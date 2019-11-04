55371b26d3d2547ae186bab1fe89088a7cc42ba3
Simara Conceição <contato@simaraconceicao.com>
2019-11-04 14:59:57 -0300

    adiciona solução do exercicio 1

---
+++ array/ex1/script.js
@@ -0,0 +1,28 @@
+// ;
+// let btn = document.getElementById("btn")
+// let input = document.getElementById("input")
+
+// console.log(btn)
+
+// btn.addEventListener(submit, function(){
+//     let arrayProcura = frutas.filter((fruta)=> fruta === input.value)
+//     console.log(arrayProcura)
+// })
+
+let frutas = ["banana", "morango", "bacurí", "laranja"]
+
+let input = document.getElementById('input')
+console.log(input)
+let btn = document.getElementById("btn")
+console.log(btn)
+
+btn.addEventListener("click", function(){
+    let arrayProcura = frutas.filter((fruta)=> fruta === input.value)
+    console.log(arrayProcura)
+    
+    if(arrayProcura.length>0){
+        console.log("temos essa fruta!")
+    } else{
+        console.log("não temos essa fruta")
+    }
+})
